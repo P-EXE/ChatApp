74531481f04e84481bf730b0fed5881a3fa33dd2
Paul <110413835+P-EXE@users.noreply.github.com>
2024-04-08 08:48:09 +0200

    Configured Program; Added SQLDBContext; Added blank User Model;

---
+++ ChatApi/DataContexts/SQLDBContext.cs
@@ -0,0 +1,22 @@
+﻿using ChatApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApi.DataContexts
+{
+  public class SQLDBContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+  {
+    DbSet<User> Users => Set<User>(); 
+    public SQLDBContext(DbContextOptions<SQLDBContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      builder.Entity<User>();
+    }
+  }
+}
--- ChatApi/Program.cs
+++ ChatApi/Program.cs
@@ -1,3 +1,8 @@
+using ChatApi.DataContexts;
+using ChatApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace ChatApi;
 
@@ -12,7 +17,29 @@ public class Program
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddSwaggerGen(options =>
+    {
+      options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+      {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+      });
+      options.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
+
+    builder.Services.AddDbContext<SQLDBContext>(options =>
+      options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ChatDB-SQLConnection")
+      )
+    );
+
+    builder.Services.AddAuthorization();
+    builder.Services.AddIdentityApiEndpoints<User>()
+      .AddEntityFrameworkStores<SQLDBContext>();
+
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
     var app = builder.Build();
 
@@ -23,10 +50,11 @@ public class Program
       app.UseSwaggerUI();
     }
 
+    app.MapGroup("/api/user").MapIdentityApi<User>();
+
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
-
 
     app.MapControllers();
 
